3b9f6751d75f74f6c5d3706a89a3e32b99148dfc
Bruno Novais <nb25372@MAC130735.local>
2019-04-15 17:50:10 +0100

    restaurants animation

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Restaurant } from './restaurant.model'
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-20px, -10px)'}),
+        animate('100ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready'
 
   /* sempre que há uma propriedade que vai receber um dado é ncessário o decorator Input */
   /* Restaurant é o tipo, que é importado do ficheiro RestaurantComponent.model */
 
-  @Input()  restaurant: Restaurant 
+  @Input() restaurant: Restaurant 
 
   constructor() { }
 
