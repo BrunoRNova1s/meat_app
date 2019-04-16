cfd54c9a7cb266175b58094ffdae034d7a57df49
Bruno Novais <nb25372@MAC130735.local>
2019-04-16 15:23:55 +0100

    search bar restaurants

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -3,21 +3,71 @@ import { Component, OnInit } from "@angular/core";
 import { Restaurant } from "./restaurant/restaurant.model";
 import { RestaurantsService } from "./restaurants.service";
 
+import {
+  trigger,
+  state,
+  transition,
+  style,
+  animate
+} from "@angular/animations";
+import { FormBuilder, FormGroup, FormControl } from "@angular/forms";
+
 @Component({
   selector: "mt-restaurants",
-  templateUrl: "./restaurants.component.html"
+  templateUrl: "./restaurants.component.html",
+  animations: [
+    trigger("toggleSearch", [
+      state(
+        "hidden",
+        style({
+          opacity: 0,
+          "max-height": "0px"
+        })
+      ),
+      state(
+        "visible",
+        style({
+          opacity: 1,
+          "max-height": "70px",
+          "margin-top": "20px"
+        })
+      ),
+      transition("* => *", animate("250ms 0s ease-in-out"))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+  searchBarState = "hidden";
   restaurants: Restaurant[];
 
-  constructor(private restaurantsService: RestaurantsService) {}
+  searchForm: FormGroup;
+  searchControl: FormControl;
+
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private fb: FormBuilder
+  ) {}
 
   /* lifecycle do componente */
   ngOnInit() {
-    this.restaurantsService.restaurants()
-      .subscribe(restaurants => {
-        this.restaurants = restaurants;
-        console.log('-----data RESTAURANTES-----', restaurants)
-      })
+    this.searchControl = this.fb.control("");
+    this.searchForm = this.fb.group({
+      searchControl: this.searchControl
+    });
+
+    /* saber o que é digitado | quando valor muda*/
+    this.searchControl.valueChanges.subscribe(searchTerm =>
+      console.log("searchTerm", searchTerm)
+    );
+
+    this.restaurantsService.restaurants().subscribe(restaurants => {
+      this.restaurants = restaurants;
+      console.log("-----data RESTAURANTES-----", restaurants);
+    });
+  }
+
+  toggleSearch() {
+    this.searchBarState =
+      this.searchBarState === "visible" ? "hidden" : "visible";
   }
 }
